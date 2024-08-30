7bec13fd40d8129660484f6c22fcafaf000d3689
deptrainhatgai <23010038@st.phenikaa-uni.edu.vn>
2024-08-30 11:30:18 +0700

    baì tập tuần 2

---
+++ main/java/com/mycompany/app/Book.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+public class Book {
+    private String ten;
+    private String tacGia;
+    public Book(String ten, String tacGia) {
+        this.ten = ten;
+        this.tacGia = tacGia;
+    }
+    public String getTen() {
+        return ten;
+    }
+    public String getTacGia() {
+        return tacGia;
+    }
+}
---
+++ main/java/com/mycompany/app/NameNumber.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+public class NameNumber {
+    private String name;
+    private int number;
+
+    public NameNumber(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
